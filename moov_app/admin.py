1f9e6b7432f521ec334ec48ecb7f700d9e4afb19
Your Name <=>
2021-07-22 21:43:58 -0500

    adds firsst commit

--- moov_app/admin.py
+++ moov_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+# admin.site.register(Photo)
+from .models import FloorPlan, Furniture, Photo
+
+admin.site.register(FloorPlan)
+admin.site.register(Furniture)
+admin.site.register(Photo)
