3249a1dadcb16c86135da788a5d3c827f49091fd
James Perey <jamjam.perey@gmail.com>
2021-07-22 20:56:49 -0700

    WIP:added carousel functionality

--- moov_app/urls.py
+++ moov_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('accounts/signup/', views.signup, name='signup'),
@@ -11,3 +13,5 @@ urlpatterns = [
     path('furniture/create/', views.FurnitureCreate.as_view(), name="add_furniture"),
     path('floorplandemo/', views.floorplan_demo, name='demo'),
 ]   
+
+urlpatterns += staticfiles_urlpatterns()
